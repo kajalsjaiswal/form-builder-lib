eed9646fe0f749a87d053b9a7959001c8f3c1f2c
kajalsjaiswal <kajal7.dev@gmail.com>
2025-07-03 06:19:51 +0000

    json of default fields and schema and folder structure

---
+++ src/types/form.ts
@@ -0,0 +1,48 @@
+export interface FormField {
+    id: string;
+    type: string;
+    name: string;
+    label?: string;
+    placeholder?: string;
+    description?: string;
+    fields?: any;
+    value?:any;
+    props?: {
+        required?: boolean;
+        readOnly?: boolean;
+        disabled?: boolean;
+        min?: number;
+        max?: number;
+        step?: number;
+        float?: boolean;
+        href?: string;
+        target?: "_blank" | "_self";
+        accept?: string;
+        rows?: number;
+        maxLength?: number;
+        level?: number;
+        fontSize?: number;
+        align?: string;
+        bold?: boolean;
+        italic?: boolean;
+        errorMsg?: string;
+        visibility?: string;
+    };
+    options?: {
+        label: string;
+        value: string | number;
+        checked?: boolean;
+    }[];
+    defaultValue?: any;
+    condition?: {
+        field: string;
+        value: any;
+    };
+    validation?: {
+        pattern?: string;
+        errorMessage?: string;
+        minLength?: number;
+        maxLength?: number;
+        custom?: string;
+    };
+}
